a3bca0064ee76f1913c3d63c4b7655d3195a9937
reham <hijazireham@gmail.com>
2019-11-04 01:21:43 +0300

    Add first Assignment

---
+++ app/src/main/java/com/example/firstform/SecondForm.java
@@ -0,0 +1,14 @@
+package com.example.firstform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_form);
+    }
+}
